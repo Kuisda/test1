e87197a4395f58193e1f16b06492fad205151095
Kuisda <yili4113@gmail.com>
2022-03-10 09:37:55 +0800

    python下递归

---
+++ 589. N 叉树的前序遍历/589. N 叉树的前序遍历.py
@@ -0,0 +1,27 @@
+import queue
+from typing import List
+from queue import Queue
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        ans = []
+        
+        def dfs(node:'Node'):
+            if node is None:
+                return
+            ans.append(node.val)
+            for ch in node.children:
+                dfs(ch)
+
+        dfs(root)
+
+        return ans            
+
+
+
