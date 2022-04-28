40f3fa836110db6e3f4a8b1525494573b952508f
Kuisda <yili4113@gmail.com>
2022-04-28 10:59:38 +0800

    两次遍历

---
+++ 905. 按奇偶排序数组/905. 按奇偶排序数组.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        res = []
+        for num in nums:
+            if num%2==0:
+                res.append(num)
+        for num in nums:
+            if num%2==1:
+                res.append(num)
+        return res
