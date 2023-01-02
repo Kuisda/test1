27a1bb4b55d1035ccea83aeb7ca4ca4f545f27a7
Kuisda <yili4113@gmail.com>
2023-01-02 11:02:17 +0800

    ..

---
+++ 855. 考场就座/1.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+
+class ExamRoom(object):
+    def __init__(self, n):
+        self.n = n
+        self.s = [0 for x in range(n)]
+    def seat(self):
+        """
+        :rtype: int
+        """
+    def leave(self, p):
+        """
+        :type p: int
+        :rtype: None
+        """
