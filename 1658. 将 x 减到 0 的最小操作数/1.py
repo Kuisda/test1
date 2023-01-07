ebc3da87efd6391a302a04286bc7d358497d0bf2
Kuisda <yili4113@gmail.com>
2023-01-07 13:40:03 +0800

    sliding window

---
+++ 1658. 将 x 减到 0 的最小操作数/1.py
@@ -0,0 +1,19 @@
+from typing import List
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+        n = len(nums)
+        left,right =-1,0
+        total = sum(nums)
+        step  = n+1 #初值设为n+1是为了面对sum(nums)>x的情况
+
+        if total<x:
+            return -1
+        while left<n-1:
+            while right<n and total>x:
+                total-=nums[right]
+                right+=1
+            if total==x:
+                step = min(step,(n-right)+(left+1))
+            left+=1
+            total+=nums[left]
+        return -1 if step>n else step
