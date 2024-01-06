a158a66c4e41717b331548bb44ba100b2ab80855
Kuisda <yili4113@gmail.com>
2024-01-06 13:44:24 +0800

    1944

---
+++ 1944/1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> canSeePersonsCount(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> stack;
+        vector<int> res(n, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            int h = heights[i];
+            while (!stack.empty() && stack.back() < h) {
+                stack.pop_back();
+                res[i] += 1;
+            }
+            if (!stack.empty()) {
+                res[i] += 1;
+            }
+            stack.push_back(h);
+        }
+        return res;
+    }
+};
+
+int main(){
+    Solution c;
+    vector<int> nums{10,6,8,5,11,9};
+    c.canSeePersonsCount(nums);
+}
