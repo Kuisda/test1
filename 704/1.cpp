73b36883761008e6069c176eabb89fa79e5b6869
Kuisda <yili4113@gmail.com>
2024-03-16 11:26:06 +0800

    704

---
+++ 704/1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0,right = nums.size()-1;
+        while(left<= right){
+            int mid = (left + right)/2;
+            if(nums[mid]==target)return mid;
+            else if(nums[mid]>target) right = mid - 1;
+            else left = mid + 1;
+        }
+        return -1;
+    }
+};
+
+int main(){
+    Solution c;
+    vector<int> nums{5};
+    c.search(nums,5);
+}
